8fc2a5f87adc8cecbc9498dbf9ecc16088015530
OligoCodes <josephoseibonsu742@gmail.com>
2025-06-13 01:46:40 +0000

    Create config.js
    config

---
+++ config.js
@@ -0,0 +1,43 @@
+/**
+
+
+ Copyright (C) 2025.
+ Licensed under the  GPL-3.0 License;
+ You may not sell this script.
+ It is supplied in the hope that it may be useful.
+ * @project_name : Free Bot script
+ * @author : Joseph Bonsu <https://github.com/oligocodes>
+ * @description : A Multi-functional whatsapp bot script.
+ * @version 3.0.0
+ **/
+
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "private",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/2yqhtS8Z/file-00000000f43861f89d1c7f28d361a103.png",
+MENU_IMG: process.env.MENU_IMG || "https://i.postimg.cc/765nFVKh/file-0000000072a861f89e02c20d4c7fa759.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "_Hi 🤖 Oligo-MD here, How Can I Assist You. Am alive 👾 Now 🔥._",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+FAKE_TYPING: process.env.FAKE_TYPING || "false",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
+HEART_REACT: process.env.HEART_REACT || "false",
+OWNER_REACT: process.env.OWNER_REACT || "false",
+BOT_NAME: process.env.BOT_NAME || "『 ✟ Oligo-MD ✟ 』",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
